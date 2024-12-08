1237ffd6fe9ba259425d3bc5b4a860ac0de257cb
Maria <maria.Alekseeva14@mail.ru>
2024-12-08 19:50:10 +0300

    fix3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,37 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        test: "readonly",
+        expect: "readonly",
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect", // Автоматически определяет версию React
+      },
+    },
+    rules: {
+      "no-restricted-syntax": [
+        "error",
+        "LabeledStatement",
+        "WithStatement",
+      ],
+      "no-unused-vars": "off",
+      "quotes": ["error", "double"],
+    },
+  },
+  
+  // конфигурация для плагинов
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
